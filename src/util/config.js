f8d6f5cbcfd7ab6e11ed51cf7fd5d5d28586de59
Zorehi <jeremy.legrix@hotmail.com>
2021-11-09 03:16:24 +0100

    Suppression du fichier config.js

--- src/util/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	TOKEN: 'ODg1NjA4NTc0MjcxNzU4NDA3.YTphQQ.W5PlmOBQsW9Am6s8SbCrTAwFW2Q',
-	MONGOSTRING: 'mongodb+srv://BotHero:4BuwKasiHUKFE0No@herobot.yggec.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-}
